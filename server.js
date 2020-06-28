ceb908ef090fa1efca3181eb9ee40b87668b2226
Rosa <rosaislp@gmail.com>
2020-06-27 20:33:44 -0500

    adding router, and creating path to get and post

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const router = express.Router(); 
 
 let app = express();  
+app.use(router);
 
-app.use('/', (req, res) => {
-    res.send('Hola')
-});
+// Haciendo un get 
+router.get('/message', (req, res) => {
+  res.send('Lista de mensajes');
+})
+
+router.post('/message', (req, res) => {
+  res.send('Mensaje añadido');
+})
+// app.use('/', (req, res) => {
+//     res.send('Hola')
+// });
 
 app.listen(3000);
 console.log('La app está escuchando en el puerto 3000');
