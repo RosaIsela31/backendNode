c05b972c2dd2df26d36b1f5b0f2b36c256378973
Rosa <rosaislp@gmail.com>
2020-07-14 16:01:26 -0500

    add function to get successful

--- components/user/network.js
+++ components/user/network.js
@@ -13,4 +13,14 @@ router.post('/', (req, res) => {
     });
 });
 
+router.get('/', (req, res) => {
+  controller.listUsers()
+    .then(users => {
+        response.success(req, res, users, 200);
+    })
+    .catch(err => {
+      response.error(req, res, 'Internal error', 500, err)
+    });
+});
+
 module.exports = router;
